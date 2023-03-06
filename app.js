ec0f065fe09628001475082f64f5e432299d18aa
sumedh27 <56512410+sumedh27@users.noreply.github.com>
2023-03-06 20:55:23 +0530

    main

---
+++ app.js
@@ -0,0 +1,57 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = todo => {
+
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+         </li>
+    `;
+
+    list.innerHTML += html;
+
+}
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const todo = addForm.add.value.trim();
+    console.log(todo);
+
+    if(todo.length){
+        generateTemplate(todo);
+        addForm.reset();
+    }
+});
+
+// Delete todos
+
+list.addEventListener('click', e =>{
+
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = term => {
+    
+    Array.from(list.children)
+        .filter(todo => !todo.textContent.toUpperCase().includes(term))
+        .forEach( todo => todo.classList.add('filtered'))
+
+    Array.from(list.children)
+        .filter(todo => todo.textContent.toUpperCase().includes(term))
+        .forEach( todo => todo.classList.remove('filtered'))
+
+}
+
+// Keyup Event
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toUpperCase();
+    filterTodos(term);
+
+})
